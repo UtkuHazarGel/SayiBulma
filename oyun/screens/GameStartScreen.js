575db59717325e6eef893b758b09c3fb84300a8b
UtkuHazarGel <155973196+UtkuHazarGel@users.noreply.github.com>
2024-01-10 22:21:14 +0300

    Oyun içindeki sayfalar oluşturuldu

---
+++ oyun/screens/GameStartScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function GameStartScreen() {
+  return (
+    <View>
+      <Text>GameStartScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
