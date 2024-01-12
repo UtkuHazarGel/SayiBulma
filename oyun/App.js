91e0d8390400dbee9f00b400fb849dcb00aef2ae
Utku <utkuhazargel@gmail.com>
2024-01-12 17:23:13 +0300

    arka plana resim verdik. ama daha uygun renk bulduğum için sadece renk kaldı.

--- oyun/App.js
+++ oyun/App.js
@@ -1,16 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import GameStartScreen from "./screens/GameStartScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-  return <GameStartScreen />;
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={["rgba(0,100,250,0.8)", "transparent"]}
+    >
+      <ImageBackground
+        style={styles.container}
+        source={require("./assets/back.png")}
+      imageStyle={styles.backImage}>
+        <GameStartScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "lightblue",
     alignItems: "center",
     justifyContent: "center",
   },
+  backImage:{
+    opacity:0.0,
+
+
+  }
 });
