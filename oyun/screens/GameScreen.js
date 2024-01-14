ed4a2e68db6212eab4b8fb8bef8684e87ad2b0c5
Utku <utkuhazargel@gmail.com>
2024-01-15 01:19:40 +0300

    artı ve eksi butonları eklendi. Artı ve eksiye göre yeni tahmniler üretiyor.

--- oyun/screens/GameScreen.js
+++ oyun/screens/GameScreen.js
@@ -1,7 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import Title from "../components/Title";
+import ComputerNumber from "../components/ComputerNumber";
+import CustomButton from "../components/CustomButton";
 
+
+let minNumber = 1;
+let maxNumber = 100;
 export default function GameScreen({ userNumber }) {
   const initialGuess = generateNumber(1, 100, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
@@ -14,12 +19,42 @@ export default function GameScreen({ userNumber }) {
       return randomNumber;
     }
   }
+
+  function nextGuessHandler(direction) {
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "greater" && currentGuess > userNumber)
+    ) {
+      Alert.alert(
+        "Bence yanlış gidiyorsun",
+        "Diğer seçeneği denemeye ne dersin?",
+        [{ text: "Tamam", style: "cancel" }]
+      );
+      return;
+    }
+ 
+    if (direction === "lower") {
+      maxNumber = currentGuess;
+    } else {
+      minNumber = currentGuess + 1;
+    }
+    const newRandomNumber = generateNumber(minNumber, maxNumber, currentGuess);
+    setCurrentGuess(newRandomNumber);
+  }
   return (
     <View style={styles.container}>
       <Title> Bilgisayar Tahmini</Title>
-      <Text>{currentGuess}</Text>
+      <ComputerNumber>{currentGuess}</ComputerNumber>
       <View>
         <Text>Altında mı? Üstüne mi?</Text>
+        <View>
+          <CustomButton onPress={nextGuessHandler.bind(this, "lower")}>
+            -
+          </CustomButton>
+          <CustomButton onPress={nextGuessHandler.bind(this, "greater")}>
+            +
+          </CustomButton>
+        </View>
       </View>
     </View>
   );
